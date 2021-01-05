6d5617b6b8fa460427e4fa6481020688a51ce589
hikobe8 <hikobe8@163.com>
2021-01-05 11:28:16 +0800

    杨辉三角

---
+++ src/main/java/basic/other/PascalsTriangle.java
@@ -0,0 +1,50 @@
+package basic.other;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *   118. 杨辉三角
+ *   给定一个非负整数 numRows，生成杨辉三角的前 numRows 行。
+ *   在杨辉三角中，每个数是它左上方和右上方的数的和。
+ *
+ * 示例:
+ *
+ * 输入: 5
+ * 输出:
+ * [
+ *      [1],
+ *     [1,1],
+ *    [1,2,1],
+ *   [1,3,3,1],
+ *  [1,4,6,4,1]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/pascals-triangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class PascalsTriangle {
+
+    /**
+     * 时间复杂度O(numRows*numRows)
+     * @param numRows
+     * @return
+     */
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(result.get(i - 1).get(j - 1) + result.get(i - 1).get(j));
+                }
+            }
+            result.add(row);
+        }
+        return result;
+    }
+
+}
