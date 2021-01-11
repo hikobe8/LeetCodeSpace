ed6879a79ce2a101bd18bfc72201c53010ae35d0
hikobe8 <hikobe8@163.com>
2021-01-11 14:28:40 +0800

    无重复字符的最长子串

---
+++ src/main/java/hot100/LengthOfLongestSubstring.java
@@ -0,0 +1,73 @@
+package hot100;
+
+import java.util.HashMap;
+
+/**
+ * 3. 无重复字符的最长子串
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: s = "abcabcbb"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+ * 示例 2:
+ *
+ * 输入: s = "bbbbb"
+ * 输出: 1
+ * 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+ * 示例 3:
+ *
+ * 输入: s = "pwwkew"
+ * 输出: 3
+ * 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+ *      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ * 示例 4:
+ *
+ * 输入: s = ""
+ * 输出: 0
+ *  
+ *
+ * 提示：
+ *
+ * 0 <= s.length <= 5 * 104
+ * s 由英文字母、数字、符号和空格组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LengthOfLongestSubstring {
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(lengthOfLongestSubstring("bbbbb"));
+        System.out.println(lengthOfLongestSubstring("pwwkew"));
+        System.out.println(lengthOfLongestSubstring(""));
+        System.out.println(lengthOfLongestSubstring("123a3]{"));
+        System.out.println(lengthOfLongestSubstring("au"));
+        System.out.println(lengthOfLongestSubstring("dvdf"));
+        System.out.println(lengthOfLongestSubstring("aaabcdeeee"));
+        System.out.println(lengthOfLongestSubstring("abbca"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int len = s.length();
+        int start = 0;
+        int end = 0;
+        int max = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
+        while (end < len) {
+            if (map.containsKey(s.charAt(end))) {
+                start = Math.max(map.get(s.charAt(end)) + 1, start);
+            }
+            map.put(s.charAt(end), end);
+            end++;
+            max = Math.max(max, end - start);
+        }
+        return max;
+    }
+
+}
