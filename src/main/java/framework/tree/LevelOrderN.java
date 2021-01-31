8610050b94be1cf40dff9b1bd5a4868b4ed9eba7
hikobe8@163.com <hikobe8@163.com>
2021-01-31 16:42:29 +0800

    429. N 叉树的层序遍历 BFS

---
+++ src/main/java/framework/tree/LevelOrderN.java
@@ -0,0 +1,52 @@
+package framework.tree;
+
+import data_structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 429. N 叉树的层序遍历
+ */
+public class LevelOrderN {
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                level.add(node.val);
+                if (node.children != null)
+                    queue.addAll(node.children);
+            }
+            res.add(level);
+        }
+        return res;
+    }
+
+}
