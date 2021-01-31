8b74f7d799675ecc59614d7c0a51ce566e3b138d
hikobe8@163.com <hikobe8@163.com>
2021-01-31 16:24:05 +0800

    515. 在每个树行中找最大值 BFS

---
+++ src/main/java/framework/tree/LargestValues.java
@@ -0,0 +1,56 @@
+package framework.tree;
+
+import data_structure.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 515. 在每个树行中找最大值
+ * 您需要在二叉树的每一行中找到最大的值。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入:
+ * <p>
+ * 1
+ * / \
+ * 3   2
+ * / \   \
+ * 5   3   9
+ * <p>
+ * 输出: [1, 3, 9]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LargestValues {
+
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                max = Math.max(max, node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(max);
+        }
+        return res;
+    }
+
+}
