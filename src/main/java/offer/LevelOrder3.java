a8a5842dc9f8e7270a6c8d7d524ed707d0d95c7c
hikobe8 <hikobe8@163.com>
2021-02-23 00:01:10 +0800

    剑指 Offer 32 - III. 从上到下打印二叉树 III

---
+++ src/main/java/offer/LevelOrder3.java
@@ -0,0 +1,70 @@
+package offer;
+
+import data_structure.TreeNode;
+
+import java.util.*;
+
+/**
+ * 剑指 Offer 32 - III. 从上到下打印二叉树 III
+ * 请实现一个函数按照之字形顺序打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右到左的顺序打印，第三行再按照从左到右的顺序打印，其他行以此类推。
+ *
+ *  
+ *
+ * 例如:
+ * 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层次遍历结果：
+ *
+ * [
+ *   [3],
+ *   [20,9],
+ *   [15,7]
+ * ]
+ *  
+ *
+ * 提示：
+ *
+ * 节点总数 <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LevelOrder3 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (root == null)
+            return res;
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean reverse = false;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            LinkedList<Integer> level = new LinkedList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (reverse) {
+                    level.offerFirst(node.val);
+                } else {
+                    level.offer(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(level);
+            reverse = !reverse;
+        }
+        return res;
+    }
+
+}
